665f02ab881839c957ae8d7b2979d8526d061bab
Philip Purwoko <philippurwoko123@gmail.com>
2020-12-12 22:17:36 +0700

    :star: Solved day 12 part 1

---
+++ Day 12 Rain Risk/day12.py
@@ -0,0 +1,51 @@
+with open('input.txt') as file:
+	commands = [[i.strip()[0],int(i.strip()[1:])] for i in file.readlines()]
+
+
+class Ship:
+	def __init__(self,facing):
+		self.point_of_compass = ['N','E','S','W']
+		self.facing = facing
+		self.coor = [0,0]
+
+	def show_pos(self):
+		print(f'Ship Coordinates : {self.coor}, Facing : {self.facing}')
+		return self
+
+	def rotate(self, direction, degree):
+		if direction == 'R':
+			new_direction = (self.point_of_compass.index(self.facing) + (degree//90))%4
+			self.facing = self.point_of_compass[new_direction]
+		elif direction == 'L':
+			new_direction = (self.point_of_compass.index(self.facing) - (degree//90))%4
+			self.facing = self.point_of_compass[new_direction]
+
+	def move(self, distance, direc = ''):
+		direction = self.facing if direc == '' else direc
+		if direction == 'E':
+			self.coor[0] += distance
+		elif direction == 'W':
+			self.coor[0] -= distance
+		elif direction == 'N':
+			self.coor[1] -= distance
+		elif direction == 'S':
+			self.coor[1] += distance
+
+	def go(self,command,scale):
+		if command in self.point_of_compass:
+			self.move(distance=scale,direc=command)
+		elif command in ['L','R']:
+			self.rotate(command,scale)
+		elif command == 'F':
+			self.move(distance=scale)
+			
+			
+ship = Ship('E')
+ship.show_pos()
+
+for command in commands:
+	ship.go(command[0],command[1])
+ship.show_pos()
+
+x, y = ship.coor
+print(f'Manhattan Disctance of Ship : {abs(x)+abs(y)}')
